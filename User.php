5c4863a743f821ef011a58810700b8ac589139a7
Luca <57569295+lucavoit@users.noreply.github.com>
2024-02-28 09:00:39 +0100

    Update User.php

--- User.php
+++ User.php
@@ -5,14 +5,11 @@ class User extends DB_Model_Base{
     public $surname;
 
     public function __construct() {
-
-		global $wpdb;
-
-		$this->table_name  = $wpdb->prefix . 'baby_users';
+		$this->table_name  = 'baby_users';
 		$this->primary_key = 'id';
 	}
 
-    public function get_columns(){
+    public static function get_columns(){
         return array(
             new DB_Column("id", __('id', 'BabysittereiApplicationAdmin'), "bigint", "(19) NOT NULL AUTO_INCREMENT"),
             new DB_Column("surname", __('surname', 'BabysittereiApplicationAdmin'), "nvarchar", "(100)"),
